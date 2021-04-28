43ca0914f719c4871e045a27700ee5ec65a0a629
nathaliefc1 <nathaliefc89@gmail.com>
2021-04-28 13:50:25 +0200

    dotenv llamada y devuelve json cabecera

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import { PORT } from "./config";
 
 const app = express();
 
-const PORT = 3000;
-app.listen(()=> console.log(`Ready on port http://localhost:${PORT}`));
+app.get("/", (req, res) =>{
+    res.json({
+        cabeceras: req.headers,
+    });
+});
+
+app.listen(PORT, ()=> console.log(`✅  Ready on port http://localhost:${PORT}`));
+
