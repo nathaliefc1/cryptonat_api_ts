f1128993c86e72586e53b83968dc8f38290e5bb9
nathaliefc1 <nathaliefc89@gmail.com>
2021-05-13 15:26:45 +0200

    prepadando heroku

--- src/models/Simulator.ts
+++ src/models/Simulator.ts
@@ -2,14 +2,19 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const schema = new Schema({
-  profile_id: String,
-  startDate: Date,
-  checkDate: Date,
-  cryptocurrency: String,
-  euros: String,
-  CryptoPriceStart: Number,
-  CryptoPriceCheck: Number,
-});
+const schema = new Schema(
+  {
+    profile_id: String,
+    startDate: Date,
+    checkDate: Date,
+    cryptocurrency: String,
+    euros: String,
+    CryptoPriceStart: Number,
+    CryptoPriceCheck: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Simulator = mongoose.model("Simulator", schema);
